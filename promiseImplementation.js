344199e17cfacab8f19a1817aba9195b3127d37e
Artyom Harutyunyan <ka6rias@gmail.com>
2023-11-10 18:41:59 +0400

    Delete console logs

--- promiseImplementation.js
+++ promiseImplementation.js
@@ -83,17 +83,3 @@ class FakePromise {
     })
   }
 }
-
-
-const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve('resolved first one'), 1000);
-})
-
-p1.then((res) => {
-  console.log(res);
-  return new Promise(resolve => {
-    setTimeout(() => resolve('resolved second one'), 1000);
-  });
-}).then(res => {
-  console.log(res);
-});
